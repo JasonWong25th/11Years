f185e647d5cc1eed03606c58b47a4a86ce2f7fb2
JasonWong25th <40476306+JasonWong25th@users.noreply.github.com>
2019-10-26 14:54:27 -0700

    Fix the input manager and player script issue and reorganized the _Script folder

---
+++ ClimateChange/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GH;
+
+//To see which platform we're on
+public enum Platform
+{
+    PC,
+    Xbox,
+    Mobile,
+    Logitech
+
+}
+
+public class AcidityBleachAudio : GH.Event
+{
+    public string type;
+}
+
+public class UpdatePoints : GH.Event
+{
+    public int deltaPoints;
+}
+
+public class UpdateOceanAcidificationUI :GH.Event
+{
+    public float oceanAcidicfaction;
+}
+
+public class PauseGame : GH.Event
+{
+    public bool pause;
+}
+
+
+public class GameManager : MonoBehaviour {
+    #region Singleton
+    private static GameManager _instance;
+    public static GameManager Instance //Ensures that this is the only instance in the class
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameManager();
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    public Platform Platform = Platform.PC;
+    public bool pause = false;
+    public Player player;
+
+    [SerializeField]
+    [Range(0,40)]
+    private float m_OceanAcidfication;
+    //private float m_currentTime;
+    //private float m_WaitTimeTillDeplete = 10f;
+    [SerializeField]
+    private float healthDepletionCoEff = 1;
+    [SerializeField]
+    private float oceanExpanisionCoEff = 1;
+
+    private bool alive = true;
+    public int points = 0;
+
+    public bool finished2Objectives = false;
+
+    private bool playedAcidAudio = false;
+    private bool playedBleachAudio = false;
+
+    private void Awake()
+    {
+        EventSystem.instance.AddListener<OnFinishEvent>(ChangeOceanAcidfcation);
+        EventSystem.instance.AddListener<PlayerDie>(OnPlayerDied);
+        EventSystem.instance.AddListener<ChangeInputType>(UpdateCurrentInput);
+        EventSystem.instance.AddListener<PauseGame>(PauseGame);
+    }
+    private void OnDisable()
+    {
+        EventSystem.instance.RemoveListener<OnFinishEvent>(ChangeOceanAcidfcation);
+        EventSystem.instance.RemoveListener<PlayerDie>(OnPlayerDied);
+        EventSystem.instance.RemoveListener<ChangeInputType>(UpdateCurrentInput);
+        EventSystem.instance.RemoveListener<PauseGame>(PauseGame);
+
+    }
+
+    private void Update()
+    {
+        //m_currentTime += Time.deltaTime;
+        //if (m_currentTime >= m_WaitTimeTillDeplete)
+        //{
+        //    m_currentTime = 0.0f;
+        //}
+        //else
+        //{
+        //}
+//        player.isInControl = !pause;
+
+        #region OceanAcidicfcation
+        if (finished2Objectives && !pause)
+            {
+                m_OceanAcidfication += Time.deltaTime * oceanExpanisionCoEff;
+            }
+
+
+            if (m_OceanAcidfication > 15)
+            {
+                healthDepletionCoEff = 1.25f;
+                if (!playedBleachAudio)
+                {
+                    EventSystem.instance.RaiseEvent(new AcidityBleachAudio
+                    {
+                        type = "bleach"
+                    });
+                    playedBleachAudio = true;
+                }
+
+            }
+            if (m_OceanAcidfication > 20)
+            {
+                healthDepletionCoEff += 1.5f;
+            }
+            if (m_OceanAcidfication > 25)
+            {
+                healthDepletionCoEff += 2f;
+            }
+            if (m_OceanAcidfication > 30)
+            {
+                healthDepletionCoEff += 3f;
+            }
+
+            if (m_OceanAcidfication > 35)
+            {
+                healthDepletionCoEff += 4f;
+            }
+            if (m_OceanAcidfication > 40)
+            {
+                healthDepletionCoEff += 5f;
+            }
+
+            if (m_OceanAcidfication > 10)
+            {
+                EventSystem.instance.RaiseEvent(new OnHealth { deltaHealthAmt = -healthDepletionCoEff * Time.deltaTime });
+                if (!playedAcidAudio)
+                {
+                    EventSystem.instance.RaiseEvent(new AcidityBleachAudio
+                    {
+                        type = "acid"
+                    });
+                    playedAcidAudio = true;
+                }
+
+
+            }
+
+            EventSystem.instance.RaiseEvent(new UpdateOceanAcidificationUI { oceanAcidicfaction = m_OceanAcidfication });
+            #endregion
+        
+        if(alive == false)
+        {
+            EventSystem.instance.RaiseEvent(new CheckForRestart{ });
+        }
+
+    }
+
+    void ChangeOceanAcidfcation(OnFinishEvent onFinishObj)
+    {
+        finished2Objectives = true;
+        if (alive)
+        {
+            m_OceanAcidfication += 2.5f;
+
+        }
+    }
+
+    void OnPlayerDied(PlayerDie playerDie)
+    {
+        alive = false;
+    }
+
+    void AddPoints(UpdatePoints updatePoints)
+    {
+        points += updatePoints.deltaPoints;
+    }
+
+    void UpdateCurrentInput(ChangeInputType inputType)
+    {
+        Platform = inputType.platform;
+    }
+
+    private void PauseGame(PauseGame _pause)
+    {
+        pause = !pause;
+    }
+    
+}
---
+++ ClimateChange/Assets/_Scripts/InputManager.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GH;
+
+public class CheckForRestart : GH.Event
+{
+
+}
+
+public class MouseClickedData : GH.Event
+{
+    public bool clicked = false;
+}
+public class KeyboardPressed : GH.Event
+{
+    public float horizontal = Input.GetAxis("Horizontal");
+    public float vertical = Input.GetAxis("Vertical");
+}
+public class Rushing : GH.Event
+{
+    public bool rush = false;
+}
+
+public class Eat : GH.Event
+{
+    public bool eat = false;
+}
+
+
+
+public class InputManager : MonoBehaviour
+{
+
+    protected static InputManager _instance;
+    bool checkRestartButton = false;
+    public bool inverted = false;
+
+    public bool pasue = false;
+
+    public static InputManager Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        EventSystem.instance.AddListener<CheckForRestart>(CheckRestartButton);
+    }
+    private void OnDisable()
+    {
+        EventSystem.instance.RemoveListener<CheckForRestart>(CheckRestartButton);
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            inverted = !inverted;
+        }
+
+        if (Input.GetKey(KeyCode.P) || Input.GetKey(KeyCode.Joystick1Button3))
+        {
+            if (!pasue)
+            {
+                EventSystem.instance.RaiseEvent(new PauseGame { pause = true });
+                pasue = true;
+            }
+            else
+            {
+                EventSystem.instance.RaiseEvent(new PauseGame { pause = false });
+                pasue = false;
+            }
+        }
+        if (!inverted)
+        {
+            if (GameManager.Instance.Platform == Platform.PC)
+            {
+                EventSystem.instance.RaiseEvent(new KeyboardPressed
+                {
+                    horizontal = Input.GetAxis("Horizontal"),
+                    vertical = Input.GetAxis("Vertical")
+                });
+                //            Debug.Log("This is PC");
+            }
+            else if (GameManager.Instance.Platform == Platform.Logitech)
+            {
+                EventSystem.instance.RaiseEvent(new KeyboardPressed
+                {
+
+                    horizontal = Input.GetAxis("RightJoystickX"),
+                    vertical = Input.GetAxis("RightJoystickY")
+                });
+                Debug.Log("This is controller");
+            }
+        }
+        else
+        {
+            if (GameManager.Instance.Platform == Platform.PC)
+            {
+                EventSystem.instance.RaiseEvent(new KeyboardPressed
+                {
+                    horizontal = Input.GetAxis("Horizontal"),
+                    vertical = Input.GetAxis("Vertical") * -1
+                });
+                //            Debug.Log("This is PC");
+            }
+            else if (GameManager.Instance.Platform == Platform.Logitech)
+            {
+                EventSystem.instance.RaiseEvent(new KeyboardPressed
+                {
+
+                    horizontal = Input.GetAxis("RightJoystickX"),
+                    vertical = Input.GetAxis("RightJoystickY") * -1
+                });
+                Debug.Log("This is controller");
+            }
+        }
+        //Can proabbaly use Events but nah
+        /* switch (GameManager.Instance.Platform)
+         {
+             case (Platform.PC):
+                 EventSystem.instance.RaiseEvent(new KeyboardPressed
+                 {
+                     horizontal = Input.GetAxis("Horizontal"),
+                     vertical = Input.GetAxis("Vertical")
+                 });
+                 Debug.Log("This is PC");
+
+                 break;
+             case (Platform.Logitech):
+                 EventSystem.instance.RaiseEvent(new KeyboardPressed
+                 {
+
+                     horizontal = Input.GetAxis("RightJoystickX"),
+                     vertical = Input.GetAxis("RightJoystickY")
+                 });
+                 Debug.Log("This is controller");
+
+                 break;
+         }
+         */
+
+
+        //I edited the Input for Fires since there was an error for me if you don't have one change it back to what works
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Joystick1Button1))//Rush Button
+        {
+            //_rush = true;
+            EventSystem.instance.RaiseEvent(new Rushing
+            {
+                rush = true
+            });
+        }
+        else
+        {
+            //_rush = false;
+            EventSystem.instance.RaiseEvent(new Rushing { rush = false });
+        }
+
+        //The following isn't good enough because time needs to pass before returning back to false
+        if (Input.GetKeyUp(KeyCode.E) || Input.GetKeyUp(KeyCode.Joystick1Button2))//Eat Button
+        {
+            EventSystem.instance.RaiseEvent(new Eat { eat = true });
+        }
+        else
+        {
+            EventSystem.instance.RaiseEvent(new Eat { eat = false });
+        }
+
+        if (Input.GetButton("Fire3"))
+        {
+            //_pause = !_pause;
+        }
+
+        if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.Joystick1Button7) || Input.GetKey(KeyCode.Joystick1Button6))
+        {
+            EventSystem.instance.RaiseEvent(new MouseClickedData { clicked = true });
+        }
+        else
+        {
+            EventSystem.instance.RaiseEvent(new MouseClickedData { clicked = false });
+        }
+
+        if (checkRestartButton)
+        {
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Joystick1Button0))
+            {
+                EventSystem.instance.RaiseEvent(new Restart { });
+            }
+        }
+        if (Input.GetKey(KeyCode.M) || Input.GetKey(KeyCode.Joystick1Button2))
+        {
+            EventSystem.instance.RaiseEvent(new OnDismissCollectable { });
+        }
+
+    }
+
+    void CheckRestartButton(CheckForRestart restart)
+    {
+        checkRestartButton = true;
+    }
+}
