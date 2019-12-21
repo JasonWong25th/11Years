b58fdafbe1a59728d0d7ef3206171ffcbdfb9494
DanteBrundage <42014062+DanteBrundage@users.noreply.github.com>
2019-12-21 13:03:00 -0800

    Start Screen UI updated for all controller options

--- ClimateChange/Assets/_Scripts/LoadGame.cs
+++ ClimateChange/Assets/_Scripts/LoadGame.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class LoadGame : MonoBehaviour
 {
+    public GameObject panel;
+    public GameObject panelShadow;
+    public GameObject loadCircle;
+
+    private bool startLoad = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +19,41 @@ public class LoadGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (startLoad)
+        {
+            StartCoroutine("LoadYourAsyncScene");
+        }
     }
 
     public void LoadScene()
     {
-       SceneManager.LoadScene(2);
+        SceneManager.LoadScene(2);
+    }
+    public void AsyncLoadScene()
+    {
+        startLoad = true;
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        bool sceneLoaded;
+        panel.SetActive(false);
+        panelShadow.SetActive(false);
+        loadCircle.SetActive(true);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(2);
+        asyncLoad.allowSceneActivation = false;
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            
+            yield return null;
+        }
+
+        if (asyncLoad.isDone)
+        {
+            yield return new WaitForSeconds(3);
+            asyncLoad.allowSceneActivation = true;
+        }
     }
 }
